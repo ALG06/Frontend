cfae8e690a617aebd7b2ab9e67d019c0152d800d
Andrés Martínez <autummata@gmail.com>
2024-11-25 23:50:53 -0600

    Add: Donation system

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserService extends ChangeNotifier {
